0d29d7407e3cc907bc7aea0da6d7e4fe6796a817
Riqwan Thamir <rmthamir@gmail.com>
2015-09-16 00:05:15 +0530

    add ember files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,15 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require ember
+//= require ember-data
+//= require active-model-adapter
+//= require_self
+//= require ./ember_blog
+
+// for more details see: http://emberjs.com/guides/application/
+EmberBlog = Ember.Application.create();
+
 //= require_tree .
