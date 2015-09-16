c58b0ecacdf3bfdc87127c03b81746de178fa101
Riqwan Thamir <rmthamir@gmail.com>
2015-09-16 15:54:03 +0530

    add remove feature

--- app/assets/javascripts/controllers/post_controller.js
+++ app/assets/javascripts/controllers/post_controller.js
@@ -17,5 +17,14 @@ EmberBlog.PostController = Ember.ObjectController.extend({
       this.set('contentEditMode', false);
       this.get('model').save();
     },
+
+    removePost: function() {
+      var post = this.get('model');
+
+      post.deleteRecord();
+      post.save();
+
+      this.transitionTo('posts');
+    },
   },
 });
