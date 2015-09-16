caf6050c04e01d6f16055c27d2600e7202d0eec8
Riqwan Thamir <rmthamir@gmail.com>
2015-09-16 11:21:00 +0530

    add named outlet

--- app/assets/javascripts/routes/posts_route.js
+++ app/assets/javascripts/routes/posts_route.js
@@ -2,4 +2,8 @@ EmberBlog.PostsRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('post');
   },
+
+  renderTemplate: function() {
+    this.render({ outlet: 'blog-posts' });
+  },
 });
