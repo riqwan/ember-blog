9545d173c3b8894e722d639452b63c442e8e9bd2
Riqwan Thamir <rmthamir@gmail.com>
2015-09-18 12:34:55 +0530

    update serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -2,4 +2,10 @@ class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :content
 
   has_many :comments
+
+  def comments
+    object.comments.map do |comment|
+      comment.id
+    end
+  end
 end
